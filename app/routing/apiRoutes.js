a316b461b84eed6065a2a38312b8d277e61587cf
N <username@users.noreply.github.com>
2018-12-21 11:41:27 -0500

    match logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,27 @@ module.exports = app => {
         res.json(data);
     });
     app.post("/api/friends", (req, res) => {
-        data.push(req.body);
-        res.json(true);
+        const newFriend = req.body;
+        const bestFriend = {
+            name: "",
+            photo: ""
+        };
+        let smallestDifference = 99999999;
+
+        for (friend of data) {
+            const totalDifference = newFriend.scores.reduce((accumulator, score, i) => {
+                const difference = Math.abs(score - friend.scores[i]);
+                return accumulator + difference;
+            }, 0);
+
+            if (totalDifference < smallestDifference) {
+                smallestDifference = totalDifference;
+                bestFriend.name = friend.name;
+                bestFriend.photo = friend.photo;
+            };
+        };
+
+        data.push(newFriend);
+        res.json(bestFriend);
     });
 };
